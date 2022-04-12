ad3471164575f38bfcaef23d06fd77caca0610d3
SRVergasov <SRVergasov@stud.kpfu.ru>
2022-04-12 12:58:42 +0300

    completed linked list

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -4,16 +4,32 @@ namespace assignment {
 
   // Используя три указателя на предыдущий, текущий и следующий узлы.
   void LinkedList::ReverseIterative() {
-    // ...
+    Node* previous = nullptr;
+    Node* curr = front_;
+
+    while (curr != nullptr) {
+      Node* next = curr->next;
+      (*curr).next = previous;
+
+      previous = curr;
+      curr = next;
+    }
+    front_ = previous;
   }
 
   void LinkedList::ReverseRecursive() {
-    // ...
+    reverse_recursive_helper(front_, nullptr);
   }
 
   // Вспомогательный метод для реализации рекурсивного переворачивания списка.
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // ...
+    if (curr == nullptr) {
+      front_ = prev;
+      return;
+    }
+    Node* cur = curr->next;
+    curr->next = prev;
+    reverse_recursive_helper(cur, curr);
   }
 
 }  // namespace assignment
